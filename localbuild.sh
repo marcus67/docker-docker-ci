c13f9c1b917b2ea7e1998f5fd73d6c7c06669986
Marcus Rickert <marcus.rickert@web.de>
2019-12-29 17:56:43 +0100

    * Add tagging

--- localbuild.sh
+++ localbuild.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=release-0.9
+# See https://stackoverflow.com/questions/1404796/how-to-get-the-latest-tag-name-in-current-branch-in-git
+VERSION=$(git describe --abbrev=0)
 
-git tag -a ${VERSION} -m "Version ${VERSION}"
-git push origin --tags
 curl -k https://raw.githubusercontent.com/marcus67/python_base_app/fb_rickert_beethoven/requirements.txt -o assets/requirements.txt
 docker build -t marcusrickert/docker-docker-ci:${VERSION}  . $*
--- tag.sh
+++ tag.sh
@@ -3,6 +3,3 @@
 VERSION=release-0.9
 
 git tag -a ${VERSION} -m "Version ${VERSION}"
-git push origin --tags
-curl -k https://raw.githubusercontent.com/marcus67/python_base_app/fb_rickert_beethoven/requirements.txt -o assets/requirements.txt
-docker build -t marcusrickert/docker-docker-ci:${VERSION}  . $*
